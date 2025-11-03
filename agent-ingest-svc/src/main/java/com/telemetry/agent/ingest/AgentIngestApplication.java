06bb6026e74101db3fea01ca05e4b8bf94475bda
Pavlo Gutsa <pavlogutsa@gmail.com>
2025-11-03 11:53:08 -0500

    agent-ingest-svc first draft

---
+++ agent-ingest-svc/src/main/java/com/telemetry/agent/ingest/AgentIngestApplication.java
@@ -0,0 +1,11 @@
+package com.telemetry.agent.ingest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentIngestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AgentIngestApplication.class, args);
+    }
+}
